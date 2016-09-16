80d03775840f8f3f6b206875fae5f9fca034f9dc
vladhrytsuk <vlad-aizen@yandex.by>
2016-09-16 15:05:34 +0300

    clean code

--- src/Filter.java
+++ src/Filter.java
@@ -8,23 +8,26 @@ import java.util.Random;
 public class Filter {
     public static void main(String[] args) {
         try {
-            File f = new File("111.bmp");
-            BufferedImage input = ImageIO.read(f);
+            BufferedImage input = ImageIO.read(new File("111.bmp"));
+
             int width = input.getWidth();
             int height = input.getHeight();
-            BufferedImage image = new BufferedImage(width, height,
-                    BufferedImage.TYPE_3BYTE_BGR);
+
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+
             Graphics g = image.getGraphics();
             g.drawImage(input, 0, 0, null);
             g.dispose();
-            ImageIO.write(image, "BMP", new File("./a.bmp"));
-            getGrayscaleImageData(image);
+           // ImageIO.write(image, "BMP", new File("./a.bmp"));
+
+            addNoise(image);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static int[][] getGrayscaleImageData(BufferedImage image) throws Exception {
+    public static int[][] addNoise(BufferedImage image) throws Exception {
         Random random = new Random();
         int width = image.getWidth();
         int heigth = image.getHeight();
@@ -36,6 +39,7 @@ public class Filter {
             x = random.nextInt(width);
             y = random.nextInt(heigth);
             image.setRGB(x, y, (1 - image.getRGB(x, y) * Integer.parseInt(String.valueOf(random.nextInt(255)), 16)));
+            image.getRGB(x, y);
         }
 
         ImageIO.write(image, "BMP", new FileOutputStream("bitmap-mis.bmp"));
