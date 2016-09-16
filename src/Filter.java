0c95a72c97b09b94d37e06b60ce3cd485f5debdc
Uladzislau Hrytsuk <hrul@AD.OFFICE.INT>
2016-09-16 14:35:31 +0300

    Create project, and add Noise

---
+++ src/Filter.java
@@ -0,0 +1,44 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
+
+public class Filter {
+    public static void main(String[] args) {
+        try {
+            File f = new File("111.bmp");
+            BufferedImage input = ImageIO.read(f);
+            int width = input.getWidth();
+            int height = input.getHeight();
+            BufferedImage image = new BufferedImage(width, height,
+                    BufferedImage.TYPE_3BYTE_BGR);
+            Graphics g = image.getGraphics();
+            g.drawImage(input, 0, 0, null);
+            g.dispose();
+            ImageIO.write(image, "BMP", new File("./a.bmp"));
+            getGrayscaleImageData(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int[][] getGrayscaleImageData(BufferedImage image) throws Exception {
+        Random random = new Random();
+        int width = image.getWidth();
+        int heigth = image.getHeight();
+        int size = width * heigth;
+        int count = (size * 10) / 100;
+        int x, y;
+
+        for (int i = 0; i < count; i++) {
+            x = random.nextInt(width);
+            y = random.nextInt(heigth);
+            image.setRGB(x, y, (1 - image.getRGB(x, y) * Integer.parseInt(String.valueOf(random.nextInt(255)), 16)));
+        }
+
+        ImageIO.write(image, "BMP", new FileOutputStream("bitmap-mis.bmp"));
+        return null;
+    }
+}
